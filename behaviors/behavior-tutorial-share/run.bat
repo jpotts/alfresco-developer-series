c4a4241cc23c1f7d2b3e6b1022254aed72cdb328
jpotts <jeffpotts01@gmail.com>
2016-09-26 14:09:10 -0500

    Behaviors share project upgrade to 2.2.0.

--- behaviors/behavior-tutorial-share/run.bat
+++ behaviors/behavior-tutorial-share/run.bat
@@ -1,8 +1,3 @@
 @ECHO OFF
 
-IF "%MAVEN_OPTS%" == "" (
-    ECHO The environment variable 'MAVEN_OPTS' is not set, setting it for you
-    SET MAVEN_OPTS=-Xms256m -Xmx2G -XX:PermSize=300m
-)
-ECHO MAVEN_OPTS is set to '%MAVEN_OPTS%'
 mvn clean install -Pamp-to-war
--- behaviors/behavior-tutorial-share/run.sh
+++ behaviors/behavior-tutorial-share/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# Note. This script requires Alfresco.war to be running in another Tomcat on port 8080
+
 if [[ -z ${MAVEN_OPTS} ]]; then
     echo "The environment variable 'MAVEN_OPTS' is not set, setting it for you";
-    MAVEN_OPTS="-Xms256m -Xmx1524m -XX:PermSize=300m"
+
+    # Downloads the spring-loaded lib if not existing and runs the Share AMP applied to Share WAR
+    springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.5.RELEASE/springloaded-1.2.5.RELEASE.jar
+
+    if [ ! -f $springloadedfile ]; then
+        mvn validate -Psetup
+    fi
+
+    # Spring loaded can be used with the Share AMP project in 5.1
+    # (i.e. it does not have the same problem as Repo AMP and AIO)
+    MAVEN_OPTS="-javaagent:$springloadedfile -noverify"
 fi
 echo "MAVEN_OPTS is set to '$MAVEN_OPTS'";
 mvn clean install -Pamp-to-war
