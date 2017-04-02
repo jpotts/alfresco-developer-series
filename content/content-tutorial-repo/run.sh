478cade446b94d30af5b5e987e8b4a407e93c1b2
jpotts <jeffpotts01@gmail.com>
2017-04-02 10:54:24 -0500

    Upgrade content-tutorial-repo to SDK 3.0.0.

--- content/content-tutorial-repo/run.sh
+++ content/content-tutorial-repo/run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [[ -z ${MAVEN_OPTS} ]]; then
-    echo "The environment variable 'MAVEN_OPTS' is not set, setting it for you";
-    MAVEN_OPTS="-Xms256m -Xmx1524m -XX:PermSize=300m"
-fi
-echo "MAVEN_OPTS is set to '$MAVEN_OPTS'";
-mvn clean install -Pamp-to-war
+
+MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install alfresco:run
